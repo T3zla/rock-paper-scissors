6d967d53ec5dce858b85f64e57e45a4dbea93c61
T3zla <alzettes@yahoo.com>
2025-06-01 17:34:19 +0800

    Add game logic for 1 round of the game.

--- main.js
+++ main.js
@@ -2,13 +2,57 @@ function getComputerChoice(){
     let computerChoice = '';
     const randNum = Math.floor(Math.random() * 3 ) + 1;
     if (randNum === 1) {
-        computerChoice = "rock";
+        computerChoice = "ROCK";
     } else if (randNum === 2) {
-        computerChoice = "paper";
+        computerChoice = "PAPER";
     } else {
-        computerChoice = "scissors"
+        computerChoice = "SCISSORS";
     }
     return computerChoice;
 }
 
-console.log(getComputerChoice());
+
+
+function getHumanChoice(){
+    let humanChoice = prompt("Please choose, Rock, Paper, or Scissors: ");
+    return humanChoice;
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanSelection, computerSelection){
+  console.log(computerSelection);
+  humanSelection = humanSelection.toUpperCase();
+
+  if (humanSelection === computerSelection) {
+    console.log("It's a draw, you both chose " + humanSelection);
+  } else {
+
+    let outcome = humanSelection + "-" + computerSelection;
+    let congrats = "Congrats! the computer chose " + computerSelection + "! You win!";
+    let badLuck = "The computer chose " + computerSelection + "! You lose.";
+
+    switch (outcome) {
+      case "ROCK-SCISSORS":
+        console.log(congrats);
+        humanScore += 1;
+        break;
+      case "PAPER-ROCK":
+        console.log(congrats);
+        humanScore += 1;
+        break;
+      case "SCISSORS-PAPER":
+        console.log(congrats);
+        humanScore += 1;
+        break;
+      default:
+        console.log(badLuck);
+        computerScore += 1;
+    }
+}
+  
+}
+
+playRound(getHumanChoice(), getComputerChoice());
+

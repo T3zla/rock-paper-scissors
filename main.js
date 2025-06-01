d81bb7177bacb8d07c1225cd55617eb4dc9014fb
T3zla <alzettes@yahoo.com>
2025-06-01 18:34:59 +0800

    Add code for a full game of 5 rounds

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// This function generates the computer's choice of ROCK, PAPER or SCISSORS, it returns a string
+
 function getComputerChoice(){
     let computerChoice = '';
     const randNum = Math.floor(Math.random() * 3 ) + 1;
@@ -11,48 +13,64 @@ function getComputerChoice(){
     return computerChoice;
 }
 
-
+// This function receives the Human Choice, it returns the Human Choice as  a capitalised string.
 
 function getHumanChoice(){
     let humanChoice = prompt("Please choose, Rock, Paper, or Scissors: ");
     return humanChoice;
 }
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame(){
+  
+  let humanScore = 0;
+  let computerScore = 0;
 
-function playRound(humanSelection, computerSelection){
-  console.log(computerSelection);
-  humanSelection = humanSelection.toUpperCase();
-
-  if (humanSelection === computerSelection) {
-    console.log("It's a draw, you both chose " + humanSelection);
-  } else {
+  function playRound(humanSelection, computerSelection){
+  
+    console.log(computerSelection);
+    humanSelection = humanSelection.toUpperCase();
 
     let outcome = humanSelection + "-" + computerSelection;
-    let congrats = "Congrats! the computer chose " + computerSelection + "! You win!";
+    let congrats = "Congrats! The computer chose " + computerSelection + "! You win!";
     let badLuck = "The computer chose " + computerSelection + "! You lose.";
 
-    switch (outcome) {
-      case "ROCK-SCISSORS":
-        console.log(congrats);
-        humanScore += 1;
-        break;
-      case "PAPER-ROCK":
-        console.log(congrats);
-        humanScore += 1;
-        break;
-      case "SCISSORS-PAPER":
-        console.log(congrats);
-        humanScore += 1;
-        break;
-      default:
-        console.log(badLuck);
-        computerScore += 1;
-    }
-}
-  
+    if (humanSelection === computerSelection) {
+      console.log("It's a draw, you both chose " + humanSelection);
+    } else {
+      switch (outcome) {
+        case "ROCK-SCISSORS":
+          console.log(congrats);
+          humanScore += 1;
+          break;
+        case "PAPER-ROCK":
+          console.log(congrats);
+          humanScore += 1;
+          break;
+        case "SCISSORS-PAPER":
+          console.log(congrats);
+          humanScore += 1;
+          break;
+        default:
+          console.log(badLuck);
+          computerScore += 1;
+        }
+      } 
+  }
+
+  for (let i = 0; i < 5; i++){
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+    console.log("Human Score: " + humanScore);
+    console.log("Computer Score: " + computerScore);
+  }
 }
 
-playRound(getHumanChoice(), getComputerChoice());
+  
+
+
+
+
+playGame();
+
 

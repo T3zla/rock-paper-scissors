a50bedfea9009ea8c237e54e74c1fac61732615e
T3zla <alzettes@yahoo.com>
2025-06-25 19:58:00 +0800

    Add basic game logic with button event listeners

--- main.js
+++ main.js
@@ -12,66 +12,58 @@ function getComputerChoice(){
     return computerChoice;
 }
 
-function getHumanChoice(){
-    let humanChoice = prompt("Please choose, Rock, Paper, or Scissors: ").toUpperCase();
+function playRound(humanSelection, computerSelection){
 
-    while ( humanChoice !== "ROCK" && humanChoice !== "PAPER" && humanChoice !== "SCISSORS"){
-      humanChoice = prompt("Invalid Choice, please choose Rock, Paper or Scissors!").toUpperCase();
-    }
-    return humanChoice;
+  console.log(computerSelection);
+  humanSelection = humanSelection.toUpperCase();
+
+  let outcome = humanSelection + "-" + computerSelection;
+  let congrats = "Congrats! The computer chose " + computerSelection + "! You win!";
+  let badLuck = "The computer chose " + computerSelection + "! You lose.";
+
+  if (humanSelection === computerSelection) {
+    console.log("It's a draw, you both chose " + humanSelection);
+  } else {
+    switch (outcome) {
+      case "ROCK-SCISSORS":
+        console.log(congrats);
+        humanScore += 1;
+        break;
+      case "PAPER-ROCK":
+        console.log(congrats);
+        humanScore += 1;
+        break;
+      case "SCISSORS-PAPER":
+        console.log(congrats);
+        humanScore += 1;
+        break;
+      default:
+        console.log(badLuck);
+        computerScore += 1;
+      }
+    } 
 }
 
-function playGame(){
-  
-  let humanScore = 0;
-  let computerScore = 0;
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
 
-  function playRound(humanSelection, computerSelection){
-  
-    console.log(computerSelection);
-    humanSelection = humanSelection.toUpperCase();
+rock.addEventListener("click", () => {
+  playRound("rock",getComputerChoice())
+});
 
-    let outcome = humanSelection + "-" + computerSelection;
-    let congrats = "Congrats! The computer chose " + computerSelection + "! You win!";
-    let badLuck = "The computer chose " + computerSelection + "! You lose.";
+paper.addEventListener("click", () => {
+  playRound("paper",getComputerChoice())
+});
 
-    if (humanSelection === computerSelection) {
-      console.log("It's a draw, you both chose " + humanSelection);
-    } else {
-      switch (outcome) {
-        case "ROCK-SCISSORS":
-          console.log(congrats);
-          humanScore += 1;
-          break;
-        case "PAPER-ROCK":
-          console.log(congrats);
-          humanScore += 1;
-          break;
-        case "SCISSORS-PAPER":
-          console.log(congrats);
-          humanScore += 1;
-          break;
-        default:
-          console.log(badLuck);
-          computerScore += 1;
-        }
-      } 
-  }
-
-  for (let i = 0; i < 5; i++){
-    let humanSelection = getHumanChoice();
-    let computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-    console.log("Human Score: " + humanScore);
-    console.log("Computer Score: " + computerScore);
-  }
-}
-
+scissors.addEventListener("click", () => {
+  playRound("scissors",getComputerChoice())
+});
   
 
 
 
 
-playGame();
+
 
 
